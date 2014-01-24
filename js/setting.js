166b02bf6e9346cc2ed6eab318ab42aeb9994dca
Will Bartlett <will118@gmail.com>
2014-01-24 13:49:16 +0000

    Removed D3 going to go for a graphless dashboard the dep isn't worth it and I could do with the custom front end practice

--- js/setting.js
+++ js/setting.js
@@ -43,7 +43,7 @@ var myApp = angular.module('angulatransmissionApp')
       if (angular.isString(data)) {
         $scope.session = data;
       } else {
-        $scope.stats = data;
+        $scope.stats = data['arguments']['cumulative-stats'];
       }
     });
   };
@@ -55,21 +55,4 @@ var myApp = angular.module('angulatransmissionApp')
   }, 3337);
 
 });
-myApp.directive('bars', function ($parse) {
-  return {
-    restrict: 'E',
-    replace: true,
-    template: '<div id="chart"></div>',
-    link: function (scope, element, attrs) {
-    var data = attrs.data.split(','),
-    chart = d3.select('#chart')
-      .append("div").attr("class", "chart")
-      .selectAll('div')
-      .data(data).enter()
-      .append("div")
-      .transition().ease("elastic")
-      .style("width", function(d) { return d + "%"; })
-      .text(function(d) { return d + "%"; });
-    }
-  };
-});
+
